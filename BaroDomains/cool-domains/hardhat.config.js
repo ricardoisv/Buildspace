bd2e76968b003657d157eab151620d70e466dd35
ricardoisv <ricardoisv@gmail.com>
2022-11-19 21:11:56 -0600

    key hide

--- BaroDomains/cool-domains/hardhat.config.js
+++ BaroDomains/cool-domains/hardhat.config.js
@@ -1,16 +1,3 @@
-require("@nomiclabs/hardhat-waffle");
-
-module.exports = {
-  solidity: "0.8.17",
-  networks: {
-    mumbai: {
-      url: "https://necessary-soft-fog.matic-testnet.discover.quiknode.pro/d045a57eb00153a85b2acad538c2cc3bd471ff50/",
-      accounts: ["67c5df379d7f6f99280ac71ee0e8ec7488a3a2f99a2320f269d2a165741fda9e"],
-    }
-  }
-};
-
-
 require('@nomiclabs/hardhat-waffle');
 require('dotenv').config();
 
